167447d1be0a62cbb05bcc1ec8396fb9005fcbc1
Vishnuat <120106924+Vishnuat@users.noreply.github.com>
2024-11-25 00:03:54 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -14,26 +14,26 @@ class _BotCommands:
         self.StopAllCommand = [f"stopall{i}", "stopallbot"]
         self.ListCommand = f"list{i}"
         self.SearchCommand = f"search{i}"
-        self.StatusCommand = [f"status{i}", "statusall"]
+        self.StatusCommand = [f"status{i}", f"s{i}", "statusall"]
         self.UsersCommand = f"users{i}"
         self.AuthorizeCommand = [f"authorize{i}", f"a{i}"]
-        self.UnAuthorizeCommand = f"unauthorize{i}"
+        self.UnAuthorizeCommand = [f"unauthorize{i}", f"ua{i}"]
         self.AddSudoCommand = f"addsudo{i}"
         self.RmSudoCommand = f"rmsudo{i}"
-        self.PingCommand = "ping"
-        self.RestartCommand = [f"restart{i}", "restartall"]
+        self.PingCommand = ["ping", "p"]
+        self.RestartCommand = [f"restart{i}", f"r{i}", "restartall"]
         self.StatsCommand = [f"stats{i}", "statsall"]
-        self.HelpCommand = f"help{i}"
+        self.HelpCommand = [f"help{i}", f"h{i}"]
         self.LogCommand = f"log{i}"
         self.ShellCommand = f"shell{i}"
         self.EvalCommand = f"eval{i}"
         self.ExecCommand = f"exec{i}"
-        self.BotSetCommand = f"botsettings{i}"
-        self.UserSetCommand = f"usetting{i}"
-        self.SpeedCommand = f"speedtest{i}"
+        self.BotSetCommand = [f"botsettings{i}", f"bs{i}"]
+        self.UserSetCommand = [f"usetting{i}", f"us{i}"]
+        self.SpeedCommand = [f"speedtest{i}", f"sp{i}"]
         self.AddImageCommand = f"addimg{i}"
         self.ImagesCommand = f"images{i}"
-        self.MediaInfoCommand = f"mediainfo{i}"
+        self.MediaInfoCommand = [f"mediainfo{i}", f"mi{i}"]
         self.BroadcastCommand = [f"broadcast{i}", "broadcastall"]
 
 
